02314c5ec80a99c5aab2ae6023a237e7b86c9102
Kacper Adach <kacperadach@gmail.com>
2016-09-04 15:36:51 -0400

    renamed public methods for clarity

--- NFL_Draftkings/GetPlayerDKScores.py
+++ NFL_Draftkings/GetPlayerDKScores.py
@@ -1,19 +1,19 @@
 from ConvertToDraftKings import convert_to_DK
 from GetPlayerStats import get_player_stats, get_weekly_player_stats
 
-def get_stats(name=None, position=None, team=None, week=None, season=None):
+def get_scores(name=None, position=None, team=None, week=None, season=None):
     data = get_player_stats(name=name, position=position, team=team, week=week, season=season)
     for d in data:
         d['stats'] = convert_to_DK(d)
     return data
 
-def get_weekly_stats(name=None, position=None, team=None, weeks=None, season=None):
+def get_weekly_scores(name=None, position=None, team=None, weeks=None, season=None):
     weekly_data = get_weekly_player_stats(name=name, position=position, team=team, weeks=weeks, season=season)
     for wd in weekly_data:
        wd['stats'] = convert_to_DK(wd)
     return weekly_data
 
 def get_top_performers(position=None, team=None, week=None, season=None, top=10):
-    data = get_stats(position=position, team=team, week=week, season=season)
+    data = get_scores(position=position, team=team, week=week, season=season)
     newlist = sorted(data, key=lambda k: k['stats'], reverse=True)
     return newlist[0:top]
--- NFL_Draftkings/tests/TestGetPlayerDKScores.py
+++ NFL_Draftkings/tests/TestGetPlayerDKScores.py
@@ -4,52 +4,52 @@ from NFL_Draftkings.GetPlayerDKScores import *
 
 class TestPublicApiMethods(unittest.TestCase):
 
-    def test_get_stats_empty(self):
-        data = get_stats(season=2015)
+    def test_get_scores_empty(self):
+        data = get_scores(season=2015)
         assert len(data) == 585
 
-    def test_get_stats_name(self):
-        data = get_stats(name='Tom Brady', season=2015)
+    def test_get_scores_name(self):
+        data = get_scores(name='Tom Brady', season=2015)
         assert len(data) == 1 and data[0]['name'] == 'Tom Brady'
 
-    def test_get_stats_name_and_week(self):
-        data = get_stats(name='Tom Brady', week=1, season=2015)
+    def test_get_scores_name_and_week(self):
+        data = get_scores(name='Tom Brady', week=1, season=2015)
         assert len(data) == 1 and data[0]['name'] == 'Tom Brady'
 
-    def test_get_stats_position(self):
-        data = get_stats(position='QB', season=2015)
+    def test_get_scores_position(self):
+        data = get_scores(position='QB', season=2015)
         assert len(data) == 72 and not [x for x in data if x['position'] != 'QB']
 
-    def test_get_stats_position_and_week(self):
-        data = get_stats(position='QB', week=1, season=2015)
+    def test_get_scores_position_and_week(self):
+        data = get_scores(position='QB', week=1, season=2015)
         assert len(data) == 35 and not [x for x in data if x['position'] != 'QB']
 
-    def test_get_stats_position_and_week_and_name_and_team(self):
-        data = get_stats(name='Tom Brady', position='QB', team='NE', week=1, season=2015)
+    def test_get_scores_position_and_week_and_name_and_team(self):
+        data = get_scores(name='Tom Brady', position='QB', team='NE', week=1, season=2015)
         assert len(data) == 1 and not [x for x in data if x['position'] != 'QB']
 
-    def test_get_weekly_stats_empty(self):
-        data = get_weekly_stats(season=2015)
+    def test_get_weekly_scores_empty(self):
+        data = get_weekly_scores(season=2015)
         assert len(data) == 5960
 
-    def test_get_weekly_stats_name(self):
-        data = get_weekly_stats(name='Tom Brady', season=2015)
+    def test_get_weekly_scores_name(self):
+        data = get_weekly_scores(name='Tom Brady', season=2015)
         assert len(data) == 16 and not [x for x in data if x['name'] != 'Tom Brady']
 
-    def test_get_weekly_stats_name_and_weeks(self):
-        data = get_weekly_stats(name='Tom Brady', weeks=[1,2,3], season=2015)
+    def test_get_weekly_scores_name_and_weeks(self):
+        data = get_weekly_scores(name='Tom Brady', weeks=[1,2,3], season=2015)
         assert len(data) == 3 and not [x for x in data if x['name'] != 'Tom Brady']
 
-    def test_get_weekly_stats_position(self):
-        data = get_weekly_stats(position='WR', season=2015)
+    def test_get_weekly_scores_position(self):
+        data = get_weekly_scores(position='WR', season=2015)
         assert len(data) == 2170 and not [x for x in data if x['position'] != 'WR']
 
-    def test_get_weekly_stats_position_and_week(self):
-        data = get_weekly_stats(position='WR', weeks=[1,2,3], season=2015)
+    def test_get_weekly_scores_position_and_week(self):
+        data = get_weekly_scores(position='WR', weeks=[1,2,3], season=2015)
         assert len(data) == 402 and not [x for x in data if x['position'] != 'WR']
 
-    def test_get_weekly_stats_position_and_week_and_name_and_team(self):
-        data = get_weekly_stats(name='Julian Edelman', position='WR', team='NE', weeks=[1,2,3], season=2015)
+    def test_get_weekly_scores_position_and_week_and_name_and_team(self):
+        data = get_weekly_scores(name='Julian Edelman', position='WR', team='NE', weeks=[1,2,3], season=2015)
         assert len(data) == 3 and not [x for x in data if x['name'] != 'Julian Edelman']
 
     def test_get_top_performers_empty(self):
