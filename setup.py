8ce3ab12e7ee0067adeecdb68de90d3b625fa095
Kacper <kacperadach@gmail.com>
2016-09-04 19:55:20 -0400

    fixed pypi upload issue

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
   name = 'NFL_Draftkings',
   packages = ['NFL_Draftkings'],
-  version = '0.1',
+  version = '2',
   description = 'Python wrapper around NFL api that calculates Draftkings scores for players',
   author = 'Kacper Adach',
   author_email = 'kacperadach@gmail.com',
   url = 'https://github.com/kacperadach/NFL_Draftkings',
-  download_url = 'https://github.com/kacperadach/NFL_Draftkings/tarball/0.1',
+  download_url = 'https://github.com/kacperadach/NFL_Draftkings/tarball/2',
   keywords = ['nfl', 'draftkings', 'python', 'api', 'wrapper', 'player', 'scores', 'points'],
   classifiers = [],
 )
