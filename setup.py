19b98a1b93c72357dbc20f3d5dbb5944f2601a82
Kacper Adach <kacperadach@gmail.com>
2016-09-04 15:13:29 -0400

    random pypi changes

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'NFL_Draftkings',
   packages = ['NFL_Draftkings'],
-  version = '0.4',
+  version = '0.1',
   description = 'Python wrapper around NFL api that calculates Draftkings scores for players',
   author = 'Kacper Adach',
   author_email = 'kacperadach@gmail.com',
-  url = 'pyfl.readthedocs.org',
-  download_url = 'https://github.com/kacperadach/NFL_Draftkings/tarball/0.4',
+  url = 'https://github.com/kacperadach/NFL_Draftkings',
+  download_url = 'https://github.com/kacperadach/NFL_Draftkings/tarball/0.1',
   keywords = ['nfl', 'draftkings', 'python', 'api', 'wrapper', 'player', 'scores', 'points'],
   classifiers = [],
 )
